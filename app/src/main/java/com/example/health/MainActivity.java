f032de90e5a748a207b8e3efc41d6ebc052945a2
Glen.N.Anthick <it21096266@my.sliit.lk>
2022-10-17 21:33:10 +0530

    MAD_17/10/2022 by Glen

--- app/src/main/java/com/example/health/MainActivity.java
+++ app/src/main/java/com/example/health/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.health;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import com.example.health.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new MealFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.meals_nav:
+                    replaceFragment(new MealFragment());
+                    break ;
+                case R.id.health_nav:
+                    replaceFragment(new HealthFragment());
+                    break;
+                case R.id.workout_nav:
+                    replaceFragment(new HealthRunFragment());
+                    break;
+            }
+            return true;
+        });
     }
+  private void replaceFragment(Fragment fragment){
+      FragmentManager fragmentManager = getSupportFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+      fragmentTransaction.replace(R.id.frame_layout, fragment);
+      fragmentTransaction.commit();
+  }
 }
